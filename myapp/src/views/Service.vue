97440f33598dcc91826442d17e66a0b57a6837e1
LostGeneration1999 <masaya4426saganosen@gmail.com>
2020-11-29 16:46:40 +0900

    service画面の修正

--- myapp/src/views/Service.vue
+++ myapp/src/views/Service.vue
@@ -1,9 +1,23 @@
 <template>
-    <div>
-        <div v-for="service in services" :key="service.title">
-            <img :src="service.imageurl">
-        </div>
-    </div>
+    <v-container>
+        <v-row class="text-center">
+            <v-col v-for="service in services" :key="service.title" cols="12" sm="10" md="6" lg="6" xl="3" class="border text-center" >
+                    <v-card :href="service.github" target="_blank" class="mb-2">
+                        <v-img :src="service.imageurl" height="300px"></v-img>
+                        <v-card-title class="headline text-center">{{service.title}}</v-card-title>
+                        <v-divider class="mx-3"/>
+                        <v-card-text>
+                            {{service.description}}
+                        </v-card-text>
+                        <v-card-actions>
+                            <div v-if="service.weburl">
+                                <v-btn color="deep-purple lighten-2" text :href="service.weburl">{{service.title}}へ！</v-btn>
+                            </div>
+                        </v-card-actions>
+                    </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
